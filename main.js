38f4b485bf188f8cfe210ea647db234161a9a0b9
mirab1807 <mira.benabid@student.curtin.edu.au>
2024-11-01 18:56:30 +0800

    final coding

--- main.js
+++ main.js
@@ -17,8 +17,30 @@ const swiper = new Swiper('.mySwiper', {
   //scrolling 
   new fullpage('#fullpage', {
     menu: "#nav",
-    anchors: ["mySwiper", "nine-nine-two", "nine-one-one", "events"],
+    anchors: ["mySwiper", "seven-one-eight", "nine-one-one", "events"],
     //options here
     autoScrolling: true,
     scrollHorizontally: true
 });
+
+
+// animte heading text 
+const animationElements = document.querySelectorAll (".animate-on-scroll")
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+    } else {
+      //entry.target.classList.remove("animate");
+    }
+  })
+  {
+    threshold: 0.5
+  }
+});
+
+for (let i = 0; i < animationElements.length; i++) {
+  const el = animationElements[i];
+
+  observer.observe(el);
+}
